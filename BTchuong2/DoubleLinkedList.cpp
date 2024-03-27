2dc0794ecac2dcee5c3ac7403a3d758ce12c9cc9
Aechewhy <harryhuy2472004@gmail.com>
2024-03-27 22:33:47 +0700

    add new to btchuong2

---
+++ BTchuong2/DoubleLinkedList.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int value) : data(value), next(nullptr), prev(nullptr) {}
+};
+class DoubleLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    // Constructor to create a new double linked list
+    DoubleLinkedList() : head(nullptr), tail(nullptr) {}
+    // Destructor to destroy a list
+    ~DoubleLinkedList()
+    {
+        while (head)
+        {
+            Node *curr = head;
+            head = head->next;
+            delete curr;
+        }
+    }
+    // Check if the list is empty
+    bool isEmpty()
+    {
+        return head == nullptr && tail == nullptr;
+    }
+    // get the size of the list
+    //Complexity: O(n)
+    int size()
+    {
+        int count = 0;
+        Node *curr = head;
+        while (curr->next)
+        {
+            count++;
+            curr = curr->next;
+        }
+        return count;
+    }
+    // Print All Nodes
+    void printAll()
+    {
+        if (isEmpty())
+        {
+            cout << "There is no element in the queue" << endl;
+            return;
+        }
+        int index = 1;
+        Node *curr = head;
+        while (curr != nullptr)
+        {
+            cout << "Element " << index << ":\tvalue: " << curr->data << endl;
+            curr = curr->next;
+            index++;
+        }
+    }
+    // Insert element at the front of the list
+    void insertFront(int value)
+    {
+        Node *newNode = new Node(value);
+        if (isEmpty())
+            head = tail = newNode;
+        else
+        {
+            head->prev = newNode;
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    // Insert element at the back of the list
+    void insertBack(int value)
+    {
+        Node *newNode = new Node(value);
+        if (isEmpty())
+            head = tail = newNode;
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+    void find_and_insert_after(int value, int newValue)
+    {
+        if (isEmpty())
+        {
+            cout << "There is no node in the list";
+            return;
+        }
+        Node *forward = head;
+        Node *backward = tail;
+        if (forward == backward && forward->data == value)
+        {
+            insertBack(newValue);
+            return;
+        }
+        while (forward != backward && forward->prev != backward)
+        {
+            if (forward->data == value)
+            {
+                Node *newNode = new Node(newValue);
+                newNode->next = forward->next;
+                newNode->prev = forward;
+                if (!forward->next->prev)
+                    forward->next->prev = newNode;
+                forward->next = newNode;
+                return;
+            }
+            if (backward->data == value)
+            {
+                Node *newNode = new Node(newValue);
+                newNode->next = backward->next;
+                newNode->prev = backward;
+                if (backward->next)
+                    backward->next->prev = newNode;
+                backward->next = newNode;
+                return;
+            }
+            forward = forward->next;
+            backward = backward->prev;
+        }
+        cout << "404: Not found" << endl;
+    }
+    void find_and_insert_before(int value, int newValue)
+    {
+        if (isEmpty())
+        {
+            cout << "There is no node in the list";
+            return;
+        }
+        Node *forward = head;
+        Node *backward = tail;
+        if (head->data == value)
+        {
+            insertFront(newValue);
+            return;
+        }
+        while (forward != backward && forward->prev != backward)
+        {
+            if (forward->data == value)
+            {
+                Node *newNode = new Node(newValue);
+                newNode->next = forward;
+                newNode->prev = forward->prev;
+                if (forward->prev)
+                    forward->prev->next = newNode;
+                forward->prev = newNode;
+                return;
+            }
+            if (backward->data == value)
+            {
+                Node *newNode = new Node(newValue);
+                newNode->next = backward;
+                newNode->prev = backward->prev;
+                backward->prev->next = newNode;
+                backward->prev = newNode;
+                return;
+            }
+            forward = forward->next;
+            backward = backward->prev;
+        }
+        cout << "404: Not found" << endl;
+    }
+    void deleteAll()
+    {
+        while (head)
+        {
+            Node *curr = head;
+            head = head->next;
+            delete curr;
+        }
+    }
+    void deleteFront()
+    {
+        if (isEmpty())
+            return;
+        Node *curr = head;
+        head = head->next;
+        if (head)
+            head->prev = nullptr;
+        else
+            tail = nullptr;
+        delete curr;
+    }
+    void deleteBack()
+    {
+        if (isEmpty())
+            return;
+        Node *curr = tail;
+        tail = tail->prev;
+        if (tail)
+            tail->next = nullptr;
+        else
+            head = nullptr;
+        delete curr;
+    }
+    void deleteAt(int index)
+    {
+        if (isEmpty())
+        {
+            cout << "There is no element in the list" << endl;
+            return;
+        }
+        if (index == 0)
+        {
+            deleteFront();
+            return;
+        }
+        if (index == size() - 1)
+        {
+            deleteBack();
+            return;
+        }
+        if (index < size() / 2)
+        {
+
+            Node *curr = head;
+            for (int i = 0; i < index; i++)
+                curr = curr->next;
+            Node *temp = curr;
+            curr->prev->next = curr->next;
+            curr->next->prev = curr->prev;
+            delete temp;
+            return;
+        }
+        else
+        {
+            Node *curr = tail;
+            for (int i = size(); i > index; i--)
+                curr = curr->prev;
+            Node *temp = curr;
+            curr->next->prev == curr->prev;
+            curr->prev->next == curr->next;
+            delete temp;
+            return;
+        }
+    }
+};
+int main()
+{
+    DoubleLinkedList myList;
+    for (int i = 0; i < 5; i++)
+    {
+        myList.insertBack(i);
+    }
+    // myList.insertFront(10);
+    // myList.find_and_insert_after(10, 20);
+    // myList.find_and_insert_before(10, 20);
+    // myList.deleteFront();
+    myList.deleteAt(2);
+    myList.printAll();
+    return 0;
+}
---
+++ BTchuong2/Polynomial.cpp
@@ -0,0 +1,253 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+struct Components
+{
+    double coefficients;
+    int exponents;
+    Components() : coefficients(0), exponents(0) {}
+    Components(double coeff, int exp) : coefficients(coeff), exponents(exp) {}
+};
+struct Node
+{
+    Components comp;
+    Node *next;
+    Node() : next(nullptr) {}
+    Node(Components Comp) : comp(Comp), next(nullptr) {}
+};
+class Polynomial
+{
+private:
+    Node *head;
+
+public:
+    Polynomial() : head(nullptr) {}
+    ~Polynomial()
+    {
+        while (head)
+        {
+            Node *curr = head;
+            head = head->next;
+            delete curr;
+        }
+    }
+    void printPolynomial()
+    {
+        if (head == nullptr)
+        {
+            cout << "Polynomial is empty." << endl;
+            return;
+        }
+
+        Node *curr = head;
+        while (curr)
+        {
+            if (curr->comp.coefficients != 0)
+            {
+                if (curr->comp.coefficients > 0)
+                {
+                    if (curr != head)
+                        cout << " + ";
+                    if (curr->comp.coefficients != 1)
+                        cout << curr->comp.coefficients;
+                }
+                else
+                {
+                    if (curr->comp.coefficients != -1)
+                        cout << curr->comp.coefficients;
+                    else
+                        cout << "-";
+                }
+                if (curr->comp.exponents > 0)
+                {
+                    cout << "x";
+                    if (curr->comp.exponents > 1)
+                        cout << "^" << curr->comp.exponents;
+                }
+                else if (curr->comp.exponents == 0)
+                {
+                    if (curr->comp.coefficients == 1 || curr->comp.coefficients == -1)
+                        cout << "1";
+                }
+            }
+            curr = curr->next;
+        }
+    }
+    void append(Components &comp)
+    {
+        Node *newNode = new Node(comp);
+        if (head == nullptr)
+        {
+            head = newNode;
+            return;
+        }
+
+        Node *curr = head;
+        while (curr->next != nullptr)
+        {
+            curr = curr->next;
+        }
+
+        curr->next = newNode;
+    }
+    void append(double coeff, int exp)
+    {
+        Components comp(coeff, exp);
+        append(comp);
+    }
+    void createPoly(vector<Components> comps)
+    {
+        for (int i = 0; i < comps.size(); i++)
+        {
+            append(comps[i]);
+        }
+    }
+    Node *sortTermsByExponentsDescending()
+    {
+        Node *current = head;
+        Node *sortedHead = nullptr;
+        while (current != nullptr)
+        {
+            Node *prev = nullptr;
+            Node *maxNode = current;
+            while (current->next != nullptr)
+            {
+                if (current->next->comp.exponents > maxNode->comp.exponents)
+                {
+                    prev = current;
+                    maxNode = current->next;
+                }
+                current = current->next;
+            }
+            if (prev != nullptr)
+            {
+                prev->next = maxNode->next;
+            }
+            else
+            {
+                head = maxNode->next;
+            }
+            maxNode->next = sortedHead;
+            sortedHead = maxNode;
+            current = head;
+        }
+        return sortedHead;
+    }
+};
+vector<Components> parseEquation(string equation)
+{
+    vector<Components> polynomial;
+    stringstream ss(equation);
+    string Component;
+    char op = '+';
+    while (getline(ss, Component, '+') || getline(ss, Component, '-'))
+    {
+        if (!ss.eof())
+        {
+            op = equation[ss.tellg()];
+        }
+        Components temp;
+        stringstream comp_ss(Component);
+        if (Component.find("x^") != string::npos)
+        {
+            size_t pos = Component.find("x^");
+            temp.coefficients = stod(Component.substr(0, pos));
+            temp.exponents = stod(Component.substr(pos + 2));
+        }
+        else if (Component.find("x") != string::npos)
+        {
+            size_t pos = Component.find("x");
+            temp.coefficients = stod(Component.substr(0, pos));
+            temp.exponents = 1.0;
+        }
+        else
+        {
+            temp.coefficients = stod(Component);
+            temp.exponents = 0.0;
+        }
+        if (op == '-')
+        {
+            temp.coefficients = -temp.coefficients;
+        }
+        polynomial.push_back(temp);
+    }
+    return polynomial;
+}
+Polynomial addPolynomials(Polynomial p1, Polynomial p2)
+{
+    Polynomial result;
+    Node *curr1 = p1.sortTermsByExponentsDescending();
+    Node *curr2 = p2.sortTermsByExponentsDescending();
+
+    // Iterate through both polynomials
+    while (curr1 != nullptr && curr2 != nullptr)
+    {
+        if (curr1->comp.exponents == curr2->comp.exponents)
+        {
+            result.append(curr1->comp.coefficients + curr2->comp.coefficients, curr1->comp.exponents);
+            curr1 = curr1->next;
+            curr2 = curr2->next;
+        }
+        else if (curr1->comp.exponents > curr2->comp.exponents)
+        {
+            result.append(curr1->comp.coefficients, curr1->comp.exponents);
+            curr1 = curr1->next;
+        }
+        else
+        {
+            result.append(curr2->comp.coefficients, curr2->comp.exponents);
+            curr2 = curr2->next;
+        }
+    }
+
+    // Append remaining terms of p1, if any
+    while (curr1 != nullptr)
+    {
+        result.append(curr1->comp.coefficients, curr1->comp.exponents);
+        curr1 = curr1->next;
+    }
+
+    // Append remaining terms of p2, if any
+    while (curr2 != nullptr)
+    {
+        result.append(curr2->comp.coefficients, curr2->comp.exponents);
+        curr2 = curr2->next;
+    }
+
+    return result;
+}
+void GeneratePoly(Polynomial &p)
+{
+    int exp;
+    cout << "Enter the biggest exponent: " << endl;
+    cin >> exp;
+    cout << "Enter coefficients: " << endl;
+    for (int i = 0; i <= exp; i++)
+    {
+        double coeff;
+        cout << "x^" << i << ":";
+        cin >> coeff;
+        p.append(coeff, i);
+    }
+}
+int main()
+{
+    Polynomial p1;
+    Polynomial p2;
+    GeneratePoly(p1);
+    cout << "First polynomial: ";
+    p1.printPolynomial();
+    cout << endl;
+    GeneratePoly(p2);
+    cout << "Second polynomial: ";
+    p2.printPolynomial();
+    cout << endl;
+    Polynomial sum = addPolynomials(p1, p2);
+    cout << endl
+         << "Sum: ";
+    sum.printPolynomial();
+    return 0;
+}
